01541da3a1f11b9e3f13feb21901af17f8a15cd9
kanozo12 <kanozo12@naver.com>
2020-07-15 15:52:34 +0900

    working responsive...

--- js/slide.js
+++ js/slide.js
@@ -16,5 +16,22 @@ new Swiper('.swiper-container2', {
     autoplay: {
         delay: 6000,
     },
+    breakpoints: {
+        // when window width is >= 320px
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        480: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        }
+      },
     speed: 1000,
 });
